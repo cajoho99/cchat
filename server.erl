96d72801746754ea97748efa3c1d3235654a1fa9
Carl Holmberg <holmberg.carl.j@gmail.com>
2020-02-20 17:17:00 +0100

    init

---
+++ server.erl
@@ -0,0 +1,23 @@
+-module(server).
+-export([start/1,stop/1]).
+
+% Start a new server process with the given name
+% Do not change the signature of this function.
+start(ServerAtom) ->
+    % TODO Implement function
+    % - Spawn a new process which waits for a message, handles it, then loops infinitely
+    % - Register this process to ServerAtom
+    % - Return the process ID
+    genserver:start(ServerAtom, [], fun handle/2).
+
+handle(State, Data) -> 
+    io:fwrite("~p~n~p~n", [State, Data]),
+    {reply, ok, State}.
+
+    
+% Stop the server process registered to the given name,
+% together with any other associated processes
+stop(ServerAtom) ->
+    % TODO Implement function
+    % Return ok
+    not_implemented.
